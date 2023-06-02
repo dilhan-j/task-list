ffceb46c25c02907fa8088a7ed027e9bb119f9a4
dilhan-j <129810028+dilhan-j@users.noreply.github.com>
2023-06-02 10:07:18 +1000

    Added JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    buttonClick();
+// Javascript for task planner
+
+
+// Delcare variables and assign inputs to them
+const formTask = document.getElementById("formTask");
+const taskInput = document.getElementById("taskName");
+const descriptionInput = document.getElementById("description");
+const assignToInput = document.getElementById("assignTo");
+const dueDateInput = document.getElementById("dueDate");
+const statusInput = document.getElementById("status");
+const submitForm = document.getElementById("submitForm")
+
+
+// Add event listener to button
+formTask.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("The button has been clicked");
+//    formValidation();
+inputData();
 });
-let buttonClick=() => {
-    console.log("success");
-}
+
+
+// Store inputs in taskData
+let taskData = [{}];
+
+
+// Push data into array
+let inputData = () => {
+    taskData.push({
+        taskName: taskInput.value,
+        description: descriptionInput.value,
+        assignTo: assignToInput.value,
+        dueDate: dueDateInput.value,
+        status: statusInput.value,
+    });
+    console.log(taskData);
+};
